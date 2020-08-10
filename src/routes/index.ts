92c3fc742b7df89f24a9d8bddde105097abf47db
Glauber Brack <glauber@brack.com.br>
2020-08-10 12:03:36 -0300

    creating routes to classes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,50 @@
 import express from 'express';
+import db from '../database/connection';
 
 const routes = express.Router();
 
+interface ScheduleItem {
+    week_day: number;
+    from: string;
+    to: string;
+}
 
-routes.get('/', (request, response) => {
- 
-    return response.json('Server running on port 3333')
+routes.post('/classes', async (request, response) => {
+    const {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        price,
+        schedule
+    } = request.body;
+
+    const insertedUsersIds = await db('users').insert({
+        name,
+        avatar,
+        whatsapp,
+        bio
+    });
+
+    const user_id = insertedUsersIds[0];
+
+    const insertedClassesIds = await db('classes').insert({
+        subject,
+        price,
+        user_id
+    })
+
+    const class_id = insertedClassesIds[0]
+
+    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+        return {
+            week_day: scheduleItem.week_day,
+            from: 
+        }
+    })
+
+    return response.send();
 })
 
 export default routes;
