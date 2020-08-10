a7e3f93a97612a391c9568609b86a5adc85d5446
Glauber Brack <glauber@brack.com.br>
2020-08-10 13:41:34 -0300

    abstracting methods to controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,67 +1,9 @@
 import express from 'express';
-import db from '../database/connection';
-import convertHourToMinutes from '../utils/convertHourToMinutes';
+import ClassesController from '../controllers/ClassesController';
 
 const routes = express.Router();
+const classesController = new ClassesController();
 
-interface ScheduleItem {
-    week_day: number;
-    from: string;
-    to: string;
-}
-
-routes.post('/classes', async (request, response) => {
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        price,
-        schedule
-    } = request.body;
-
-    const trx = await db.transaction();
-
-    try {
-        const insertedUsersIds = await trx('users').returning('id').insert({
-            name,
-            avatar,
-            whatsapp,
-            bio
-        });
-    
-        const user_id = insertedUsersIds[0];
-    
-    
-        const insertedClassesIds = await trx('classes').returning('id').insert({
-            subject,
-            price,
-            user_id
-        });
-    
-        const class_id = insertedClassesIds[0];
-    
-        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-            return {
-                class_id,
-                week_day: scheduleItem.week_day,
-                from: convertHourToMinutes(scheduleItem.from),
-                to: convertHourToMinutes(scheduleItem.to)
-            };
-        });
-    
-        await trx('class_schedule').insert(classSchedule);
-    
-        await trx.commit();
-    
-        return response.status(201).send({ message: 'Created successfully!'});
-    } catch (error) {
-        await trx.rollback();
-
-        return response.status(400).json({ message: 'Unexpected error whilte creating a new class.'});
-    }
-    
-})
+routes.post('/classes', classesController.store)
 
 export default routes;
