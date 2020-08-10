1deb5885d61b2539581f3a31a540d79032b170b2
Glauber Brack <glauber@brack.com.br>
2020-08-10 14:20:00 -0300

    creating connections controller and using this on routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import ClassesController from '../controllers/ClassesController';
+import ConnectionsController from '../controllers/ConnectionsController';
 
 const routes = express.Router();
-const classesController = new ClassesController();
 
-routes.get('/classes', classesController.index)
-routes.post('/classes', classesController.store)
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
+
+// CLASSES
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.store);
+
+// CONNECTIONS
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.store);
 
 export default routes;
