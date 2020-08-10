4c0f2aa66d556969876ed27711179299fd425bea
Glauber Brack <glauber@brack.com.br>
2020-08-10 10:59:02 -0300

    moving knexfile to root folder

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,4 +1,5 @@
 import knex from 'knex';
+import path from 'path';
 
 const db = knex({
     client: 'pg',
@@ -8,6 +9,11 @@ const db = knex({
         user: 'postgres',
         password: '0000'
     },
+    
+    migrations: {
+        tableName: 'knex_migrations',
+        directory: path.resolve('database', 'migrations')
+    }
 })
 
 export default db;
