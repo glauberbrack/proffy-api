9f49d8286779ff1a90b64e6156b34a844b20d769
Glauber Brack <glauber@brack.com.br>
2020-08-10 12:23:05 -0300

    using transaction and try catch block

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -21,36 +21,47 @@ routes.post('/classes', async (request, response) => {
         schedule
     } = request.body;
 
-    const insertedUsersIds = await db('users').returning('id').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio
-    });
+    const trx = await db.transaction();
 
-    const user_id = insertedUsersIds[0];
+    try {
+        const insertedUsersIds = await trx('users').returning('id').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio
+        });
+    
+        const user_id = insertedUsersIds[0];
+    
+    
+        const insertedClassesIds = await trx('classes').returning('id').insert({
+            subject,
+            price,
+            user_id
+        });
+    
+        const class_id = insertedClassesIds[0];
+    
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+            return {
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertHourToMinutes(scheduleItem.from),
+                to: convertHourToMinutes(scheduleItem.to)
+            };
+        });
+    
+        await trx('class_schedule').insert(classSchedule);
+    
+        await trx.commit();
+    
+        return response.status(201).send({ message: 'Created successfully!'});
+    } catch (error) {
+        await trx.rollback();
 
-
-    const insertedClassesIds = await db('classes').returning('id').insert({
-        subject,
-        price,
-        user_id
-    })
-
-    const class_id = insertedClassesIds[0]
-
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-        return {
-            class_id,
-            week_day: scheduleItem.week_day,
-            from: convertHourToMinutes(scheduleItem.from),
-            to: convertHourToMinutes(scheduleItem.to)
-        };
-    })
-
-    await db('class_schedule').insert(classSchedule)
-
-    return response.send();
+        return response.status(400).json({ message: 'Unexpected error whilte creating a new class.'});
+    }
+    
 })
 
 export default routes;
